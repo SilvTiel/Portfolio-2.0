b645bd25652ced429ab34dd90f4b6d19664f7d77
Sil van Tiel <silvantiel@gmail.com>
2021-05-19 13:44:55 +0200

    Over ons meer tekst

--- over.php
+++ over.php
@@ -7,8 +7,30 @@
     <title>SVT Over mij</title>
     <link rel="stylesheet" href="over.css">
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap" rel="stylesheet">
 </head>
 <body>
-    
+    <div class="header">
+        <?php include("navigatie.html");?>
+        <div class="round">
+            <img src="afbeeldingen/over/Round.png">
+            <h1>Sil van Tiel</h1>
+            <class class="mediadesign">Media & Design</class> 
+            <div class="balk">
+                <a href="https://www.instagram.com/silvtiel/" target="blank"><img src="afbeeldingen/over/instagram.png"></a>
+                <a href="mailto:silvantiel@gmail.com"><img src="afbeeldingen/over/Gmail.png"></a>
+                <a href="https://www.linkedin.com/in/sil-van-tiel-783971212/" target="blank"><img src="afbeeldingen/over/LinkedIn.png"></a>
+            </div>
+            <!-- <a href="#section1"><div class="button">bekijk project</div></a> -->
+        </div>
+        
+    </div>
+
+    <div class="block" id="section1">
+        <div class="tekst">
+            <h1>Wie ben ik?</h1>
+            <p>Heeey hoi! Mijn naam is Sil van Tiel ik ben 18 jaar jong en kom uit een klein maar gezellig dorpje Erp. Vooraf gaande aan de opleiding HBO ICT, Media & Design op de Fontys te Eindhoven heb ik de Havo afgerond op het Zwijsen College te Veghel. Destijds had ik de opleiding gekozen N&T met als keuze daarbij informatica, daar begon ik met het maken van een kleine maar simpele website. Dit vond ik destijds leuk dus wou ik hier graagin verder gaan en kijken wat er meer mee kon. Dus toen ik begon aan de opleiding wist ik bijna meteen dat ik voor Media & Design wou gaan omdat dit mij het meeste aansprak van alles waar ik uit kon kiezen. En ik zie er graag naar uit om meer over mijn opleiding te leren en alles wat hierbij komt kijken.</p>
+        </div>
+    </div>
 </body>
 </html>
